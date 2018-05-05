42aa657745126e15e3685733b42bf6ffe3dfaeea
Marco <mkkiller50@hotmail.com>
2018-05-05 14:28:35 +0200

    Add rest routes for customer and cart

--- REST.py
+++ REST.py
@@ -1,11 +1,24 @@
 from flask_restful import Resource, Api
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/customer", methods=['GET'])
+def customer():
+    return jsonify({
+        "id" : 1,
+        "name" : "customer",
+        "email" : "cusstomer@gmail.com"
+        })
 
 if __name__ == '__main__':
     app.run(debug = True)
-    
+
+@app.route("/cart", methods=['GET'])
+def cart():
+    return jsonify({
+        "id" : 1,
+        "customer_id" : 1
+        })
+
+if __name__ == '__main__':
+    app.run(debug = True)
